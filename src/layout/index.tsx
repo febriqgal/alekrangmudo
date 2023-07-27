8577cd8f9770172ac08c24540105c11e1a33302a
Febriqgal Purnama <febriqgalp@gmail.com>
2023-07-27 18:09:10 +0700

    dasdasdas

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,6 +2,7 @@ import { Poppins } from "next/font/google";
 import React from "react";
 import Footer from "./footer";
 import NavbarComponents from "./navbar";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -9,9 +10,20 @@ const poppins = Poppins({
 });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <div className={`${poppins.className}`}>
       <NavbarComponents />
+      <motion.div
+        className="bg-gradient-to-r from-primary-400  to-primary-600 bottom-0 h-1 z-[99999] rounded-full fixed left-0 right-0 origin-top"
+        style={{ scaleX }}
+      />
+
       <main>{children}</main>
       <Footer />
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import NextProgress from "next-progress";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <NextProgress
+        color="#48091a"
+        height={3}
+        delay={300}
+        options={{ showSpinner: false }}
+      />
       <Component {...pageProps} />
     </NextUIProvider>
   );
